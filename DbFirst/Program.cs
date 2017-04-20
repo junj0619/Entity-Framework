bba6a7a9297ea15cd4d065121ffbbfe4711f79ff
Jun <Jun@JUN-PC>
2017-04-19 23:17:13 -0400

    Build Pluto EF DBFirst

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace DbFirst
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermedate = 2,
+        Advanced = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Description);
+            }
+
+
         }
     }
 }
